e0c97ab3defb27f03807c30fc916962277f5f38b
kamils5 <41336621+kamils5@users.noreply.github.com>
2022-11-28 14:09:54 +0100

    Add files via upload

---
+++ forum/partials/_footer.php
@@ -0,0 +1,10 @@
+<?php
+echo 
+'
+<footer class="bg-light text-center text-lg-start">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright: Kamil Szloser
+  </div>
+</footer>
+'
+?>
